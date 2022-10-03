9aa65558143937e1a8597c82ecea27598d322cc2
Johann-Peter Hartmann <johann-peter.hartmann@mayflower.de>
2022-10-03 12:47:40 +0000

    Remove demo addons, add Dapr addon

--- tests/test-addons.py
+++ tests/test-addons.py
@@ -5,9 +5,9 @@ from utils import microk8s_enable, wait_for_pod_state, microk8s_disable
 
 
 class TestAddons(object):
-    def test_python_demo_nginx(self):
-        microk8s_enable("python-hello-k8s")
-        wait_for_pod_state("", "default", "running", label="app=python-demo-nginx")
+    def test_dapr(self):
+        microk8s_enable("dapr")
+        wait_for_pod_state("", "dapr-system", "running", label="app=dapr-dashboard")
         status = yaml.safe_load(sh.microk8s.status(format="yaml").stdout)
-        expected = {"python-hello-k8s": "enabled"}
-        microk8s_disable("python-hello-k8s")
+        expected = {"dapr": "enabled"}
+        microk8s_disable("dapr")
